4b2d137d8286f2f873c4dfa7a54070a6a086d900
Jan Barszcz <rijandael@cyber.lab>
2020-04-11 19:05:46 +0200

    missing python file

---
+++ generate.py
@@ -0,0 +1,13 @@
+import json
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('templates'))
+template = env.get_template('template.html')
+
+with open('data.json') as json_file:
+    data = json.load(json_file)
+
+rendered_template = template.render(data=data)
+
+with open("out.html", "w") as file:
+    file.write(rendered_template)
